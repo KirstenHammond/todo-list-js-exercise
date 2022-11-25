335128b2d183f5d00e4c141f4bee532a513de4c3
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2022-11-25 13:13:08 -0800

    took seperate functions and refactored them into one function containing an object and then refactored global functions to be methods

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+//Using OOP to organised a Todo list CLI app.
+
+//creating a new task
+const newTask = function(title, description) {
+  const task = {
+    title : title,
+    description : description,
+    complete : false,
+
+    logState : function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted : function() {
+      this.complete = true;
+    }
+
+  };
+  return task;
+};
+
+
+//Driver code
+const task1 = newTask("clean cat litter", "scoop poop out"); //by assigning to a variable, the result now has access to variables within the function that it was called from eg the const Task is now accessible by functions outside of its scope
+const task2 = newTask("do laundry", "washing gym clothes");
+const tasks = [task1, task2];
+
+task2.logState(); //current state
+task2.markCompleted(); //changes state from default false to true
+task2.logState(); //updated state
+
+console.log("tasks", tasks);
+
+/*
+//Existing/old code refactored above
 // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
@@ -18,7 +53,7 @@ function completeTask(taskIndex) {
 function logTaskState(taskIndex) {
   const title = taskTitles[taskIndex];
   const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
+  console.log(`${title} has${complete ? " " : " not "}been completed`);//ternary expression in the console log to print a certain message if truthy/falsey
 }
 
 // DRIVER CODE BELOW
@@ -29,3 +64,4 @@ newTask("Do Laundry"); // task 1
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+ */
